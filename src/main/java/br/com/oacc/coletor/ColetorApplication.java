c46e7fae930df47d6f49093b9b37af8ba5ff1ef4
Moisés Araújo Kalile <moises.kalile@gmail.com>
2023-06-16 14:00:11 -0300

    project-init

---
+++ src/main/java/br/com/oacc/coletor/ColetorApplication.java
@@ -0,0 +1,13 @@
+package br.com.oacc.coletor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColetorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColetorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/oacc/coletor/ColetorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oacc.coletor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColetorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
